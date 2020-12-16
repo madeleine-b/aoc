713c1f1715887152227e10e4a2ecb75fb7680f2f
Madeleine Barowsky <mbarowsky@g.harvard.edu>
2020-12-15 20:56:48 -0500

    Part 1 day 1

--- aoc1.py
+++ aoc1.py
@@ -3,8 +3,17 @@ with open("input/aoc1_input", "r") as f:
 	data = f.readlines()
 	data = [int(d.strip()) for d in data]
 
-for i in range(len(data)):
-	for j in range(i + 1, len(data)):
-		if data[i] + data[j] == 2020:
-			print(data[i]*data[j])
-			quit()
+def part_one():
+	for i in range(len(data)):
+		for j in range(i + 1, len(data)):
+			if data[i] + data[j] == 2020:
+				print(data[i]*data[j])
+				return
+
+def part_two():
+	for i in range(len(data)):
+		for j in range(i + 1, len(data)):
+			for k in range(j + 1, len(data)):
+				if data[i] + data[j] + data[k]== 2020:
+					print(data[i]*data[j]*data[k])
+					return 
