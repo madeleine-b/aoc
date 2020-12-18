f1dece1233fdb8d3b32327c5322981c29264abca
Madeleine Barowsky <mbarowsky@g.harvard.edu>
2020-12-18 18:46:23 -0500

    day 7 part 2

--- aoc7.py
+++ aoc7.py
@@ -6,14 +6,20 @@ def num_subbags_contain_gold_bag(outer_bag, rule_dict, sum_so_far):
 	sub_sum = sum_so_far
 	for rule in rule_dict[outer_bag]:
 		if rule[1] == "shiny gold":
-			#print(outer_bag, " holds shiny gold")
 			sub_sum += 1
 		else:
-			#print("making subcall for", rule[1])
-			ret = num_subbags_contain_gold_bag(rule[1], rule_dict, 0)
-			#print("\t", rule[1], " indirectly holds %d bags that hold shiny gold" % ret)
-			sub_sum += ret
+			sub_sum += num_subbags_contain_gold_bag(rule[1], rule_dict, 0)
 	return sub_sum
+
+def num_bags_within_bag(outer_bag, rule_dict):
+	if rule_dict[outer_bag] == 0:
+		return 0
+	sub_sum = 0
+	for rule in rule_dict[outer_bag]:
+		ret = num_bags_within_bag(rule[1], rule_dict)
+		ret = rule[0]*ret + rule[0]
+		sub_sum += ret
+	return sub_sum 
 
 def set_up_rule_dict():
 	rule_dict = {}
@@ -39,11 +45,17 @@ def set_up_rule_dict():
 					rule_dict[bag_kind].append((num, kind))
 	return rule_dict
 
-rule_dict = set_up_rule_dict()
-total_outer = 0
-for k, v in rule_dict.items():
-	ret = num_subbags_contain_gold_bag(k, rule_dict, 0)
-	if ret > 0:
-		total_outer += 1
+def part_one(rule_dict):
+	total_outer = 0
+	for k, v in rule_dict.items():
+		ret = num_subbags_contain_gold_bag(k, rule_dict, 0)
+		if ret > 0:
+			total_outer += 1
+	print(total_outer)
 
-print(total_outer)
+
+rule_dict = set_up_rule_dict()
+
+def part_two(rule_dict):
+	total = num_bags_within_bag("shiny gold", rule_dict)
+	print(total)
